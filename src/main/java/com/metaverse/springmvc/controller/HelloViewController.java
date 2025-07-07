7dc5218ff15644745b7f1059fb657cd44ac059d6
lusmacaron <ehdtjd366026@gmail.com>
2025-07-07 12:41:20 +0900

    feat: 뷰템플릿엔진 - 템플릿 HTML 파일 변환 방법 및 Model 객체를 통한 동적데이터바인딩

--- src/main/java/com/metaverse/springmvc/controller/HelloViewController.java
+++ src/main/java/com/metaverse/springmvc/controller/HelloViewController.java
@@ -1,10 +1,13 @@
 package com.metaverse.springmvc.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HelloViewController {
+    private static long visitCount = 0;
+
     @GetMapping("/static-hello")
     public String hello(){
         return "Hello.html";
@@ -18,6 +21,15 @@ public class HelloViewController {
     @GetMapping("/html/templates")
     public String templates(){
         // ... 내부 로직이 추가 될 수 있음
+        // prefix classpath: resources까지의 경로 +templates/까지 가지고있음
+        // suffix .html
         return "hello-templates";
+    }
+
+    @GetMapping("html/dynamic")
+    public String htmlDynamic(Model model) {
+        visitCount++;
+        model.addAttribute("visits", visitCount);
+        return "hello-dynamic";
     }
 }
