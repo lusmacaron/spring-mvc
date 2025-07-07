5f752dc896a2953c219d88e318fbb505597b5d8a
lusmacaron <ehdtjd366026@gmail.com>
2025-07-07 10:39:02 +0900

    feat: 뷰템플릿엔진 - Redirection 사용

--- src/main/java/com/metaverse/springmvc/controller/HelloViewController.java
+++ src/main/java/com/metaverse/springmvc/controller/HelloViewController.java
@@ -9,4 +9,9 @@ public class HelloViewController {
     public String hello(){
         return "Hello.html";
     }
+
+    @GetMapping("/html/redirect")
+    public String htmlRedirect(){
+        return "redirect:/hello.html";
+    }
 }
